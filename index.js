169aab83be83463d3afea3d33167edb6d3f34242
user <omjadhav963@gmail.com>
2022-07-09 23:13:58 +0530

    join game done

--- index.js
+++ index.js
@@ -21,8 +21,11 @@ mongoose.connect(db).then(() => {
     console.log(e);
 });
 
+
 io.on('connection', (socket) => {
     console.log("connected");
+
+    // CREATE GAME
     socket.on('create-game', async ({ nickname, name, occupancy, maxRounds }) => {
         try {
             const existingRoom = await Room.findOne({ name });
@@ -50,7 +53,40 @@ io.on('connection', (socket) => {
             console.log(err);
         }
     })
-})
+
+    // JOIN GAME CALLBACK
+    socket.on('join-game', async ({ nickname, name }) => {
+        try {
+            let room = await Room.findOne({ name });
+            if (!room) {
+                socket.emit('notCorrectGame', 'Please enter a valid room name');
+                return;
+            }
+
+            if (room.isJoin) {
+                let player = {
+                    socketID: socket.id,
+                    nickname,
+                }
+                room.players.push(player);
+                socket.join(name);
+
+                if (room.players.length === room.occupancy) {
+                    room.isJoin = false;
+                }
+                room.turn = room.players[room.turnIndex];
+                room = await room.save();
+                io.to(name).emit('updateRoom', room);
+            } else {
+                socket.emit('notCorrectGame', 'The game is in progress, please try later!');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    })
+});
+
+
 
 server.listen(port, "0.0.0.0", () => {
     console.log("Server running on PORT " + port);
